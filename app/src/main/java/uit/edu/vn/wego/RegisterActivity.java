d920abec47d9171a44395877ee89f559ea548690
vytran0710 <56116908+vytran0710@users.noreply.github.com>
2021-04-24 23:40:50 +0700

    Login Interface 3 & Register Interface 1

---
+++ app/src/main/java/uit/edu/vn/wego/RegisterActivity.java
@@ -0,0 +1,14 @@
+package uit.edu.vn.wego;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
