ff71b649dff2db56e087f40ed861f58243e2847c
PhungThanhTu <67744403+PhungThanhTu@users.noreply.github.com>
2021-04-28 14:07:59 +0700

    activity transition in button

--- app/src/main/java/uit/edu/vn/wego/LoginActivity.java
+++ app/src/main/java/uit/edu/vn/wego/LoginActivity.java
@@ -2,13 +2,32 @@ package uit.edu.vn.wego;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView toRegisterBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView toRegisterBtn = findViewById(R.id.to_register_button);
+
+        toRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
--- app/src/main/java/uit/edu/vn/wego/RegisterActivity.java
+++ app/src/main/java/uit/edu/vn/wego/RegisterActivity.java
@@ -2,13 +2,27 @@ package uit.edu.vn.wego;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+    TextView toLoginBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        TextView toLoginBtn = findViewById(R.id.to_login_button);
+        toLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
